f42bbbe44da1a7c7c8b0a6f3ef22595adbf29331
hzz <2857224329@qq.com>
2021-03-13 17:16:23 +0800

    修复登录功能BUG

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,15 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-=======
     <!-- 路由占位符 -->
     <router-view></router-view>
->>>>>>> login
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app'
   }
-=======
-export default {
-  name: 'app'
->>>>>>> login
-}
 </script>
 
 <style>
-<<<<<<< HEAD
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-=======
->>>>>>> login
 </style>
